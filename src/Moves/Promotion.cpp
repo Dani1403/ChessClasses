5bd6779abf2b28956c1156ddc3335be1f8ebf1a8
Daniel Sitbon <sitbondaniel@campus.technion.ac.il>
2023-08-14 00:43:25 +0300

    Implemented Promotion

--- src/Moves/Promotion.cpp
+++ src/Moves/Promotion.cpp
@@ -1,1 +1,17 @@
 #include "Promotion.h"
+#include "../ChessGame.h"
+
+bool Promotion::execute(ChessGame& game) const
+{
+	ChessBoard* board = game.getChessBoard();
+	board->removePiece(getPieceToMove());
+	board->addPiece(getNewPiece());
+	return true;
+}
+
+void Promotion::undo(ChessGame& game) const
+{
+	ChessBoard* board = game.getChessBoard();
+	board->removePiece(getNewPiece());
+	board->addPiece(getPieceToMove());
+}
