94b0abb54782c439d6760453b5b0433c54f908ce
Daniel Sitbon <sitbondaniel@campus.technion.ac.il>
2023-08-13 23:55:05 +0300

    Implemented castling using Composite Design Pattern

--- src/Moves/Castle.cpp
+++ src/Moves/Castle.cpp
@@ -1,1 +1,14 @@
 #include "Castle.h"
+#include "../ChessGame.h"
+
+bool Castle::execute(ChessGame& game) const
+{
+	m_kingMove.execute(game);
+	m_rookMove.execute(game);
+}
+
+void Castle::undo(ChessGame& game) const
+{
+	m_kingMove.undo(game);
+	m_rookMove.undo(game);
+}
--- src/Moves/Castle.h
+++ src/Moves/Castle.h
@@ -1,18 +1,28 @@
 #pragma once
 #include "ChessMove.h"
 #include "../Pieces/Rook.h"
+#include "../Pieces/King.h"
+
+enum class Side { KING, QUEEN };
 
 class Castle : public ChessMove
 {
 public:
-	Castle(const Square& source, const Square& destination, ChessPiece* kingToMove, Rook* rookToMove):
-		ChessMove(source, destination, kingToMove), m_rookToMove(rookToMove) {}
+	Castle(const Square& source, const Square& destination, King* kingToMove, Rook* rookToMove, Side side) :
+		ChessMove(source, destination, kingToMove), m_rookToMove(rookToMove), m_side(side)
+	{}
 
 	Rook* getRookToMove() const { return m_rookToMove; }
 
-	bool execute(ChessGame& game) const;
-	void undo(ChessGame& game) const;
+	bool execute(ChessGame& game) const override;
+	void undo(ChessGame& game) const override;
 private:
+	Side m_side;
 	Rook* m_rookToMove;
+	int offset() const { return m_side == Side::KING ? 2 : -2; }
+	Square kingDest() { return { getPieceToMove()->getSquare().row, getPieceToMove()->getSquare().col + offset() }; };
+	Square rookDest() { return { kingDest().row, kingDest().col - (offset() / 2) }; };
+	ChessMove m_kingMove = ChessMove(getPieceToMove()->getSquare(), kingDest(), getPieceToMove());
+	ChessMove m_rookMove = ChessMove(getRookToMove()->getSquare(), rookDest(), getRookToMove());
 };
 
