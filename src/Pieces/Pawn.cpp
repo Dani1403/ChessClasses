2bc0db048f1e9faa9de5a8c68de4b5b15ae8904b
Daniel Sitbon <sitbondaniel@campus.technion.ac.il>
2023-08-30 13:54:27 +0300

    fixed problem for pawn move

--- src/Pieces/Pawn.cpp
+++ src/Pieces/Pawn.cpp
@@ -4,9 +4,10 @@ bool Pawn::isValidMove(const Square& source, const Square& destination) const
 {
 	if (isOnSameCol(source, destination)) {
 		if (!m_hasMoved)
-			return (destination.row == source.row + 1) || (destination.row == source.row + 2);
+			return (destination.row == source.row + (m_color == Color::WHITE ? 1 : -1)) || 
+				(destination.row == source.row + (m_color == Color::WHITE ? 2 : -2));
 		else
-			return (destination.row == source.row + 1);
+			return (destination.row == source.row + (m_color == Color::WHITE ? 1 : -1));
 	}
 	return false;
 }
