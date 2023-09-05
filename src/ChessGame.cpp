71e9caf012583115feaaba3b4b453307ac14a278
Daniel Sitbon <sitbondaniel@campus.technion.ac.il>
2023-09-06 00:01:57 +0300

    cleaned ChessGame cpp

--- src/ChessGame.cpp
+++ src/ChessGame.cpp
@@ -1,5 +1,7 @@
 #include "ChessGame.h"
 
+// INITIALIZATION
+
 ChessGame::ChessGame() : m_chessBoard(std::make_shared<ChessBoard>(ChessBoard()))
 {
 	initBoard();
@@ -15,67 +17,21 @@ void ChessGame::initBoard()
 	m_chessBoard->addInitialPieces(Color::BLACK);
 }
 
-void ChessGame::moveToNextPlayer()
-{
-	m_players.push_back(m_players.front());
-	m_players.pop_front();
-	m_currentPlayer = m_players.front();
-	std::cout << "next player : " << colorToString(m_currentPlayer.getColor()) << std::endl << std::endl;
-}
+// CHECK AND CHECKMATE LOGIC ~~~~~ ENDGAME LOGIC
 
-void ChessGame::play()
+bool ChessGame::checkPossibleCapture(std::shared_ptr<Capture> capture)
 {
-	m_chessBoard->draw();
-	while (!isGameOver())
+	try
 	{
-		playerTurn();
-		moveToNextPlayer();
+		capture->checkValidity(*this);
 	}
-}
-
-bool ChessGame::makeMove(std::shared_ptr<ChessMove> move)
-{
-	move->checkValidity(*this);
-	move->execute(*this);
-	if (isInCheck(m_currentPlayer.getColor()))
+	catch (const InvalidMove& invalid)
 	{
-		displayMoveEndsInCheck();
-		move->undo(*this);
-		m_moves.pop_back();
 		return false;
 	}
 	return true;
 }
 
-void ChessGame::playerTurn()
-{
-	bool moved = false;
-	while (!moved)
-	{
-		try 
-		{
-			std::shared_ptr<ChessMove> move = m_currentPlayer.getMove(*this);
-			moved = makeMove(move);
-			if (!moved) continue;
-		}
-		catch (const InvalidMove& invalid)
-		{
-			std::cout << invalid.message() << std::endl << std::endl;
-			continue;
-		}
-	}
-	m_chessBoard->draw();
-}
-
-void ChessGame::undo()
-{
-	std::shared_ptr<ChessMove> move = m_moves.back();
-	if (move != nullptr)
-		move->undo(*this);
-	m_moves.pop_back();
-}
-
-// TODO : correct
 bool ChessGame::isInCheck(Color color)
 {
 	Square kingPos = m_chessBoard->getKingPosition(color);
@@ -116,15 +72,67 @@ bool ChessGame::isGameOver()
 	return (isInCheckmate(Color::BLACK) || isInCheckmate(Color::WHITE));
 }
 
-bool ChessGame::checkPossibleCapture(std::shared_ptr<Capture> capture)
+// MOVE AND PLAYER TURN LOGIC
+
+bool ChessGame::makeMove(std::shared_ptr<ChessMove> move)
 {
-	try
+	move->checkValidity(*this);
+	move->execute(*this);
+	if (isInCheck(m_currentPlayer.getColor()))
 	{
-		capture->checkValidity(*this);
-	}
-	catch (const InvalidMove& invalid)
-	{
+		displayMoveEndsInCheck();
+		move->undo(*this);
+		m_moves.pop_back();
 		return false;
 	}
 	return true;
+}
+
+void ChessGame::undo()
+{
+	std::shared_ptr<ChessMove> move = m_moves.back();
+	if (move != nullptr)
+		move->undo(*this);
+	m_moves.pop_back();
+}
+
+void ChessGame::playerTurn()
+{
+	bool moved = false;
+	while (!moved)
+	{
+		try
+		{
+			std::shared_ptr<ChessMove> move = m_currentPlayer.getMove(*this);
+			moved = makeMove(move);
+			if (!moved) continue;
+		}
+		catch (const InvalidMove& invalid)
+		{
+			std::cout << invalid.message() << std::endl << std::endl;
+			continue;
+		}
+	}
+	m_chessBoard->draw();
+}
+
+void ChessGame::moveToNextPlayer()
+{
+	m_players.push_back(m_players.front());
+	m_players.pop_front();
+	m_currentPlayer = m_players.front();
+	std::cout << "next player : " << colorToString(m_currentPlayer.getColor()) << std::endl << std::endl;
+}
+
+
+// MAIN LOOP 
+
+void ChessGame::play()
+{
+	m_chessBoard->draw();
+	while (!isGameOver())
+	{
+		playerTurn();
+		moveToNextPlayer();
+	}
 }
