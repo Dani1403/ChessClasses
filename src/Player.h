40d4e4c281b350aa6756cb932633e482df341416
Daniel Sitbon <sitbondaniel@campus.technion.ac.il>
2023-11-22 19:17:33 +0200

    added comment for Player.h

--- src/Player.h
+++ src/Player.h
@@ -14,10 +14,25 @@ public:
 
 	Color getColor() const { return m_color; }
 
+	/*
+	 * Let the player input a move.
+	 * @param is - the input stream to read from.
+	 * @return a string representing the move the player wants to make.
+	 */
 	std::string getMoveFromUser(std::istream& is) const;
 
+	/*
+	 * Get a move from the player.
+	 * @param game - the game to get the move for.
+	 * @return a move the player wants to make.
+	 */
 	std::shared_ptr<ChessMove> getMove(ChessGame& game) const;
 
+	/*
+	 * Compare two players by their color.
+	 * @param other - the other player to compare to.
+	 * @return true if the players have the same color, false otherwise.
+	 */
 	bool operator!=(const Player& other) const { return this->m_color != other.getColor(); }
 
 private:
