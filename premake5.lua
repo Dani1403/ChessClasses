4e75da810f8b34f8105a41d12b45c968acbf238b
Daniel Sitbon <sitbondaniel@campus.technion.ac.il>
2023-12-22 02:27:56 +0200

    vendor directory

--- premake5.lua
+++ premake5.lua
@@ -1,18 +1,28 @@
 workspace "ChessClasses"
     configurations { "Debug", "Release" }
+    architecture "x64"
 
 project "ChessClasses"
     kind "ConsoleApp"
     language "C++"
     targetdir "bin/%{cfg.buildcfg}"
 
-    files { "src/**.cpp"}
+    files { "src/**.cpp", "src/**.h"}
 
     filter "configurations:Debug"
         symbols "On"
 
     filter "configurations:Release"
         optimize "On"
+
+    links
+    {
+        "sfml-graphics",
+		"sfml-window",
+		"sfml-system"
+        "sfml-audio",
+        "sfml-network"
+    }
 
     filter "system:macosx"
         xcodebuildsettings {
