4a1732e178c90b7cf98f234319a10a82e8df8bc0
Daniel Sitbon <sitbondaniel@campus.technion.ac.il>
2024-01-10 13:42:23 +0200

    interface for a card

--- src/Cards/Card.h
+++ src/Cards/Card.h
@@ -1,3 +1,18 @@
 #pragma once
 
+#include "../Pieces/ChessPiece.h"
 #include "../utilities.h"
+
+class ChessGame;
+
+class Card
+{
+public:
+	Card() = default;
+	virtual ~Card() = default;
+	virtual void apply(ChessGame& game) const = 0;
+	virtual void undo(ChessGame& game) const = 0;
+	virtual void render(sf::RenderWindow& window) const = 0;
+	virtual std::string getName() const = 0;
+	virtual std::string getDescription() const = 0;
+};
